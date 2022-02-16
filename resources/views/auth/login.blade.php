1726dddeb476d0faea8df36818325f1b5f29b80a
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-02-16 14:37:55 +0700

    Ready to build!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,12 +50,6 @@
                 <x-button class="ml-3">
                     {{ __('Log in') }}
                 </x-button>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}"
-                        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-1"> Register
-                    </a>
-                @endif
             </div>
         </form>
     </x-auth-card>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('REST API Website Pemerintah Kota Bogor') }}
         </h2>
     </x-slot>
 
